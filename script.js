65770604c459b40366d4899ad4555c5d782cf39f
lidhill <alelid95gmail.com>
2023-10-23 18:37:07 +0200

    my own code for background generator

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+var button = document.getElementById("random");
 
-color1.addEventListener("input", function () {
-    console.log(color1.value);
-});
+function setGradient() {
+    body.style.background =
+        "linear-gradient(to right, "
+        + color1.value
+        + ", "
+        + color2.value
+        + ")";
 
-color2.addEventListener("input", function () {
-    console.log(color2.value);
-});
+    css.textContent = body.style.background + ";";
+}
+
+function random_hex_color_code() {
+    var random1 = (Math.random() * 0xfffff * 1000000).toString(16);
+    color1.value = '#' + random1.slice(0, 6);
+    var random2 = (Math.random() * 0xfffff * 1000000).toString(16);
+    color2.value = '#' + random2.slice(0, 6);
+    setGradient();
+};
+
+button.addEventListener("click", random_hex_color_code);
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
